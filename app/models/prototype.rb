6f9a0314cb62a7f3c6f318bf6e0e1afd0e7eaecb
tomo7716 <t19870106.ak@gmail.com>
2025-06-20 21:17:20 +0900

    修正依頼箇所を修正(6/20分)

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -7,6 +7,11 @@ class Prototype < ApplicationRecord
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
+  validate  :image_presence
+
+  def image_presence
+    errors.add(:image, "を選択してください") unless image.attached?
+  end
 
   def was_attached?
     self.image.attached?
